7048e6091d085f294dec9d70aae043188a375ed8
Marat <m.brezin@yandex.ru>
2025-06-05 22:28:55 +0300

    change gitignore

--- String/build/CMakeFiles/String.dir/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/String.dir/main.cpp.o: \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/share/asan_ignorelist.txt \
-  /Users/marat/CppProject/String/main.cpp
