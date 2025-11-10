a92f4a9f37f5823fd581cca636f6a1055f29da42
Marat <m.brezin@yandex.ru>
2025-11-10 21:46:53 +0300

    delete cache

--- Queue/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake
+++ Queue/cmake-build-debug/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/Users/marat/Cpp/Queue")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/marat/Cpp/Queue/build")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/Users/marat/Cpp/Queue/cmake-build-debug")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
